7f30164f558d2c065f34ebe121605fa668cd6e3b
not-so-good <sanjin.suljanovic@gmail.com>
2021-03-12 13:40:18 +0100

    ex06 Constructing string with variables

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -1,7 +1,7 @@
 function main(){
     //Only change code below this line
-    var myName = "Jerry"
-    var myString = "My name is " + myName + " and I am well!"
+    var myName = "Jerry";
+    var myString = "My name is " + myName + " and I am well!";
     //Only change code above this line 
     return myString;
 }
