6c4a2d36ea559d67f2b7605cade5ec780d785f39
not-so-good <sanjin.suljanovic@gmail.com>
2021-03-12 14:32:16 +0100

    ex10 String immutability

--- ex10/src/index.js
+++ ex10/src/index.js
@@ -3,7 +3,7 @@ function main(){
     var myString = "Jello World";
     myString = "Hello World";
     //Only change code above this line 
-    return myString;;
+    return myString;
 }
 
 console.log(main());
