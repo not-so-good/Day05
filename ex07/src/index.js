7a3a1dd0b37caee50bb647699ee65280b65ece3c
not-so-good <sanjin.suljanovic@gmail.com>
2021-03-12 13:42:33 +0100

    ex07 Appending variables to strings

---
+++ ex07/src/index.js
@@ -0,0 +1,11 @@
+function main(){
+    //Only change code below this line
+    var someAdjective = "the Best!";
+    var myString = "Mistral is ";
+    myString += someAdjective;
+    //Only change code above this line 
+    return myString;
+}
+
+console.log(main());
+module.exports = main;
